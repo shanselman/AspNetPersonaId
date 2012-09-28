405b45e8501cb99793846dcc6fc1ceb4c8ac198b
Scott Hanselman <scott@hanselman.com>
2012-09-28 11:18:03 -0700

    Getting there

--- MVC4/PersonaMVC4Example/Controllers/PersonaController.cs
+++ MVC4/PersonaMVC4Example/Controllers/PersonaController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 
 namespace PersonaMVC4Example.Controllers
@@ -12,10 +16,33 @@ namespace PersonaMVC4Example.Controllers
     {
         // POST api/persona
         [HttpPost][ActionName("login")]
-        public void Login(string assertion)
+        public async Task<HttpResponseMessage> Login(string assertion)
         {
+            if (assertion == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
             var cookies = Request.Headers.GetCookies();
             string token = cookies[0]["__RequestVerificationToken"].Value;
+
+            using (var client = new HttpClient())
+            {
+                var content = new FormUrlEncodedContent(
+                        new Dictionary<string, string> {
+                            { "assertion", assertion },
+                            { "audience", HttpContext.Current.Request.Url.Host } 
+                            //TODO: Can I get this without HttpContext.Current?
+                        }
+                    );
+                var result = await client.PostAsync("https://verifier.login.persona.org/verify", content);
+                var stringresult = await result.Content.ReadAsStringAsync();
+                dynamic jsonresult = JsonConvert.DeserializeObject<dynamic>(stringresult);
+                if (jsonresult.status == "okay")
+                {
+                    return new HttpResponseMessage(HttpStatusCode.OK);
+                }
+            }
+            return new HttpResponseMessage(HttpStatusCode.Forbidden);
         }
 
         [HttpPost][ActionName("logout")]
